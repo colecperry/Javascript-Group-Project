680b325ceb6e77fd17f99d7fd7f8dec35569a8e6
Cole Perry <colecperry@gmail.com>
2023-03-07 14:41:01 -0800

    Push for Cindy

--- src/index.js
+++ src/index.js
@@ -7,46 +7,37 @@ const fetchAll = () => {
   fetch(`${cocktailURL}`)
     .then((response) => response.json())
     .then((cocktails) => {
-      console.log(cocktails.drinks);
       const cocktails1 = cocktails.drinks;
       cocktails1.forEach((cocktail) => {
-        console.log(cocktail);
         renderCocktails(cocktail);
       });
     });
 };
 
-const renderCocktails = (cocktail) => {
+const renderCocktails = (btn) => {
   const cocktailMenu = document.querySelector("#cocktail-bar");
   const btn = document.createElement("button");
   btn.innerText = cocktail.strDrink;
+  btn.addEventListener("click", (event) => {
+  console.log("event");
+  renderDetails(cocktail)});
+}
   cocktailMenu.appendChild(btn);
+
+
+const renderDetails = (cocktail) => {
+  const image = document.querySelector("#image");
+  image.src = cocktail.strImageSource
+  image.alt = cocktail.strDrink
+  cocktailInfo.appendChild(image);
+  const name = document.querySelector("#name");
+  name.innerText = cocktail.strDrink;
+  cocktailInfo.appendChild(name);
+
 };
-
-/* const renderDetails = (cocktail) => {
-    btn.addEventListener("click", () => renderDetails(cocktail));
-    cocktails1.forEach(cocktail) => {
-    const image = document.createElement("image");
-    image.src = cocktail.strDrinkThumb
-    cocktailInfo.appendChild(image);
-    const name = document.querySelector("#name")
-    name.innerText = cocktail.strDrink
-    const ingredients = createElement("p")
-    const ingredent1 = cocktail.strIngredient1, cocktail.strMeasure1
-    const ingredent2 = cocktail.strIngredient2, cocktail.strMeasure2
-    const ingredent3 = cocktail.strIngredient3, cocktail.strMeasure3
-    const ingredent4 = cocktail.strIngredient4, cocktail.strMeasure4
-    ingredients.innerText = "ingredient1", "ingredient2", "ingredient3", "ingredient4"
-    const 
-    cocktailInfo.appendChild(name);
-    cocktailInfo.appendChild(ingredients);
-
-    }
-
-    } */
 
 const init = () => {
   fetchAll();
-  renderCocktails();
+
 };
 init();
