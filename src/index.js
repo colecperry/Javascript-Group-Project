aba5393615c7b4c7d9a3e2e0d607146222369737
Cole Perry <colecperry@gmail.com>
2023-03-07 07:49:02 -0800

    updated code

---
+++ src/index.js
@@ -0,0 +1,29 @@
+//fetch api key for 10 cocktails
+const cocktailURL =
+  "https://www.thecocktaildb.com/api/json/v2/9973533/latest.php";
+const fetchAll = () => {
+  fetch(`${cocktailURL}`)
+    .then((resp) => resp.json())
+    .then((cocktails) => {
+      cocktails.forEach((cocktailObj) =>{
+        renderCocktails(cocktails);
+      });
+    });
+};
+
+/*fetch("https://www.thecocktaildb.com/api/json/v2/9973533/latest.php")
+  .then((resp) => resp.json())
+  .then((cocktails) => renderCocktails(cocktails));
+
+// render cocktail names to the top of the page
+
+/*const renderCocktails = (cocktails) => {
+  cocktails.forEach((cocktail) => {
+    const cocktailMenu = document.querySelector("#cocktail-bar");
+    const span = document.createElement("span");
+    span.innerText = drinks.strDrink;
+    cocktailMenu.appendChild(span);
+  });
+};
+
+//"https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=margarita"
